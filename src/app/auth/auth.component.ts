2da8a26d507ea7c8239200554746299893465201
samsamhuynh <uyenhuynh13122000@gmail.com>
2024-07-06 16:58:44 +0700

    switching between auth mode

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  selector: "app-auth",
+  templateUrl: "./auth.component.html",
+  styleUrl: "./auth.component.css",
 })
 export class AuthComponent {
+  isLoginMode = true;
 
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
 }

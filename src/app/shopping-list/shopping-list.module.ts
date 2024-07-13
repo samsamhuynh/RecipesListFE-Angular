0d9a7e1ffc1e5a27bd340b93244f7326ef96f32b
samsamhuynh <uyenhuynh13122000@gmail.com>
2024-07-13 21:22:14 +0700

    adding shared module

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from "@angular/core";
 import { ShoppingListComponent } from "./shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
 import { RouterModule } from "@angular/router";
-import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [ShoppingListComponent, ShoppingEditComponent],
   imports: [
-    CommonModule,
     FormsModule,
+    SharedModule,
     RouterModule.forChild([
       { path: "shopping-list", component: ShoppingListComponent },
     ]),
